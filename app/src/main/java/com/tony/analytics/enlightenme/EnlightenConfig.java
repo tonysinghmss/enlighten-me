9563a460a78ea3e62c4e2e27d2918b63da467ec1
Manishankar Singh <manishankarsngh007@gmail.com>
2016-05-12 16:17:42 +0530

    Change in redirect uri and scope

--- app/src/main/java/com/tony/analytics/enlightenme/EnlightenConfig.java
+++ app/src/main/java/com/tony/analytics/enlightenme/EnlightenConfig.java
@@ -4,10 +4,9 @@ package com.tony.analytics.enlightenme;
  * Created by TONY on 5/12/2016.
  */
 public class EnlightenConfig {
-    public static final String OAUTH_URL = "enlightenme ouath url";
-    public static final String REDIRECT_URI = "enlightenme redirect uri";
-    public static final String OAUTH_SCOPE = "enlightenme oauth scope";
+    public static final String OAUTH_URL = "https://api.instagram.com/oauth/authorize/";
+    public static final String REDIRECT_URI = "http://enlightenme.analytics.tony.com/";
+    public static final String OAUTH_SCOPE = "basic public_content follower_list comments relationships likes";
     public static final String CLIENT_ID = "enlightenme client id";
     public static final String CLIENT_SECRET = " enlightenme client secret";
-    public static final String REDIRECT_URL = "redirect url in instagram";
 }
